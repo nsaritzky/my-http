063572ab180bb8f25af6ea6c5e5311c60618c2ba
nsaritzky <nsaritzky@gmail.com>
2023-10-31 10:34:55 -0700

    Attempt stage 2

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
-use std::net::TcpListener;
+use anyhow::Result;
+use tokio::io::AsyncWriteExt;
+use tokio::net::TcpListener;
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<()> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-
-    for stream in listener.incoming() {
-        match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
+    let listener = TcpListener::bind("127.0.0.1:4221").await?;
+    match listener.accept().await {
+        Ok((mut stream, _)) => {
+            println!("accepted new connection");
+            stream.write_all(b"HTTP/1.1 200 OK/r/n/r/n").await?;
+        }
+        Err(e) => {
+            println!("error: {}", e);
         }
     }
+    Ok(())
 }
